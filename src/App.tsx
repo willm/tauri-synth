d8a1e60379734637085428d3fe69ea3bfe9dee45
Will Munn <will.munn@focusrite.com>
2020-11-26 17:22:20 +0000

    Fix the faders a bit and add delay wet fader

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Keyboard } from "./components/Keyboard";
 import * as tauriEvent from "tauri/api/event";
+import styled from "styled-components";
 if (!(window as any).__TAURI_INVOKE_HANDLER__) {
   (window as any).__TAURI_INVOKE_HANDLER__ = () => {};
 }
@@ -10,6 +11,40 @@ interface NoteOnEvent {
   payload: {
     note: number;
   };
+}
+
+const Slider = styled.input`
+  transform: rotate(-90deg);
+  height: 70%;
+  margin-top: 20px;
+`;
+
+interface FaderProps {
+  id: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+}
+
+const FaderContainer = styled.div`
+  width: 19%;
+`;
+
+function Fader(props: FaderProps) {
+  return (
+    <FaderContainer>
+      <Slider
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        onChange={props.onChange}
+        id={props.id}
+      ></Slider>
+      <label style={{ width: "100%", height: "10%" }} htmlFor={props.id}>
+        {props.name}
+      </label>
+    </FaderContainer>
+  );
 }
 
 function App() {
@@ -28,52 +63,54 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ flexGrow: 1 }}>
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            style={{ backgroundColor: "#22222" }}
-            onChange={(e) => {
-              tauriEvent.emit("attack", e.target.value);
-              console.log(e.target.value);
-            }}
-          ></input>
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            style={{ backgroundColor: "#22222" }}
-            onChange={(e) => {
-              tauriEvent.emit("decay", e.target.value);
-              console.log(e.target.value);
-            }}
-          ></input>
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            style={{ backgroundColor: "#22222" }}
-            onChange={(e) => {
-              tauriEvent.emit("sustain", e.target.value);
-              console.log(e.target.value);
-            }}
-          ></input>
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            style={{ backgroundColor: "#22222" }}
-            onChange={(e) => {
-              tauriEvent.emit("release", e.target.value);
-              console.log(e.target.value);
-            }}
-          ></input>
-          {loaded ? <Keyboard selectedKey={midiNote}></Keyboard> : <h2>Loading ...</h2>}
+        <div style={{ flexGrow: 1, display: "flex" }}>
+          {loaded ? (
+            <div style={{ height: "200px", display: "flex" }}>
+              <Fader
+                onChange={(e) => {
+                  tauriEvent.emit("attack", e.target.value);
+                  console.log(e.target.value);
+                }}
+                id="attack"
+                name="attack"
+              ></Fader>
+              <Fader
+                onChange={(e) => {
+                  tauriEvent.emit("decay", e.target.value);
+                  console.log(e.target.value);
+                }}
+                id="decay"
+                name="decay"
+              ></Fader>
+              <Fader
+                onChange={(e) => {
+                  tauriEvent.emit("sustain", e.target.value);
+                  console.log(e.target.value);
+                }}
+                id="sustain"
+                name="sustain"
+              ></Fader>
+              <Fader
+                onChange={(e) => {
+                  tauriEvent.emit("release", e.target.value);
+                  console.log(e.target.value);
+                }}
+                id="release"
+                name="release"
+              ></Fader>
+              <Fader
+                onChange={(e) => {
+                  tauriEvent.emit("delay_wet", e.target.value);
+                  console.log(e.target.value);
+                }}
+                id="delay-wet"
+                name="delay wet"
+              ></Fader>
+              <Keyboard selectedKey={midiNote}></Keyboard>
+            </div>
+          ) : (
+            <h2>Loading ...</h2>
+          )}
         </div>
       </header>
     </div>
